467cba17f544e59a72605b060c3b153c2a574483
renuka-matta <renukamatta7@gmail.com>
2025-07-04 12:27:47 +0530

    Update medtrack.py

--- projectfiles/medtrack.py
+++ projectfiles/medtrack.py
@@ -22,7 +22,7 @@ SMTP_SERVER = 'smtp.gmail.com'
 SMTP_PORT = 587
 SENDER_EMAIL = 'your_email@gmail.com'
 SENDER_PASSWORD = 'your_app_password'
-SNS_TOPIC_ARN = 'arn:aws:sns:us-east-1:xxxxxxxxxxxx:YourSNSTopic'  # Optional
+SNS_TOPIC_ARN = arn:aws:sns:us-east-1:975050261480:medtrack:7ecaf092-a355-44bc-8600-2e3968a10f4e  # Optional
 
 # ---------------- EMAIL FUNCTION ----------------
 def send_email(to_email, subject, body):
